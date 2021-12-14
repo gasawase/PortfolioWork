ea1a6776541690c0bcf58e4be55401c9ea4905af
Summer <gasawase@mail.uc.edu>
2021-12-14 07:01:52 -0500

    BigPush

---
+++ Assets/ShaderScenes/MimicStealth/controlStealth.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlStealth : MonoBehaviour
+{
+    public GameObject surfaceGameObject;
+    public GameObject jointsGameObject;
+    public GameObject normalSurfGO;
+    public GameObject normalJointsGO;
+
+    public void stealthActivated()
+    {
+        //TODO: control cutoffs of normal and invisible, turns off the normal when almost at zero
+    }
+}
